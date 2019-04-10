d9559bed9c8d26cc6ac2adf52ece2c4c1bdfceb5
yhhu <yuzhongzi91@sina.com>
2019-04-10 19:35:59 +0800

    [#N/A]: add two models: constant & password.

--- lib/database/password_db_provider.dart
+++ lib/database/password_db_provider.dart
@@ -4,7 +4,7 @@ import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
-class DBHelper {
+class PasswordDBProvider {
   static Database _database;
   final String _tableName = "Password";
 

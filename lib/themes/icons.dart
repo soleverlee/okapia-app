f0b438ad1a8efc8826ada1213e29aed93e60de98
yhhu <yuzhongzi91@sina.com>
2019-05-06 09:49:51 +0800

    [feature]: update icons.

--- lib/themes/icons.dart
+++ lib/themes/icons.dart
@@ -5,4 +5,7 @@ class AliIcon {
   static const group = IconData(0xebd8, fontFamily: 'Ali');
   static const toolbox = IconData(0xeb8f, fontFamily: 'Ali');
   static const settings = IconData(0xeb8d, fontFamily: 'Ali');
+  static const listItem = IconData(0xeb9d, fontFamily: 'Ali');
+  static const search = IconData(0xeb9c, fontFamily: 'Ali');
+  static const help = IconData(0xeb89, fontFamily: 'Ali');
 }
